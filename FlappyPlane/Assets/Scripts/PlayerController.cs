3d2346f618cd1767668afc1028a18e64bf3440d7
Luiz Bruno <luiz_seya@hotmail.com>
2022-01-25 04:40:04 -0300

    Limitando a velocidade do player

--- FlappyPlane/Assets/Scripts/PlayerController.cs
+++ FlappyPlane/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : MonoBehaviour
     // Pegando as informações do player
     // Criando as nossas variáveis
     private Rigidbody2D meuRB;
+
+    // Velocidade
+    [SerializeField] private float velocidade = 5f;
     
     // Start is called before the first frame update
     void Start()
@@ -20,6 +23,18 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Subindo ao apertar espaço
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Fazendo a velocidade do RB ir para cima
+            meuRB.velocity = Vector2.up * velocidade;
+        }
+
+        // Limitando a minha velocidade de queda
+        if (meuRB.velocity.y < -velocidade)
+        {
+            // Travando meuRB.velocity.y em -5
+            meuRB.velocity = Vector2.down * velocidade;
+        }
     }
 }
