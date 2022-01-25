5f36f86d5c4aa54a03474baa632d24dd2f96651d
Luiz Bruno <luiz_seya@hotmail.com>
2022-01-25 05:01:17 -0300

    Iniciando o obstáculo

--- FlappyPlane/Assets/Scripts/PlayerController.cs
+++ FlappyPlane/Assets/Scripts/PlayerController.cs
@@ -24,17 +24,28 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Subindo ao apertar espaço
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            // Fazendo a velocidade do RB ir para cima
-            meuRB.velocity = Vector2.up * velocidade;
-        }
+        Subir();
 
+        LimitandoVelocidade();
+    }
+
+    private void LimitandoVelocidade()
+    {
         // Limitando a minha velocidade de queda
         if (meuRB.velocity.y < -velocidade)
         {
             // Travando meuRB.velocity.y em -5
             meuRB.velocity = Vector2.down * velocidade;
+        }
+    }
+
+    // Criando o método subir
+    public void Subir()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Fazendo a velocidade do RB ir para cima
+            meuRB.velocity = Vector2.up * velocidade;
         }
     }
 }
