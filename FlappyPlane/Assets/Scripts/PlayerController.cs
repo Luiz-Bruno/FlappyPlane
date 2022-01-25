7322cf0675b799cffa43f3117721feb064d8eec0
Luiz Bruno <luiz_seya@hotmail.com>
2022-01-25 02:30:39 -0300

    Pegando o Rigidbody do player

--- FlappyPlane/Assets/Scripts/PlayerController.cs
+++ FlappyPlane/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Pegando as informações do player
+    // Criando as nossas variáveis
+    private Rigidbody2D meuRB;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Pegando meu RB sozinho
+        meuRB = GetComponent<Rigidbody2D>();
+
+
     }
 
     // Update is called once per frame
